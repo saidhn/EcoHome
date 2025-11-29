93066fa737a7a88b4b2be48010df6fbf6b28c67c
Abedalla Emara <abedallaemara24@gmail.com>
2025-11-29 12:48:31 +0200

    last edit PUSH

--- ecohome/app/Components/hero.tsx
+++ ecohome/app/Components/hero.tsx
@@ -1,18 +1,20 @@
-import Image from "next/image";
 import Link from "next/link";
 const Hero = () => {
   return (
     <section className="relative w-full h-[80vh]">
-      <Image
-        src="/Hero Img.jpg"
-        alt="Hero"
-        fill
-        className="object-cover"
-        priority
-      />
+      {/* فيديو الخلفية */}
+  <video
+    src="/HERO.mp4"
+    autoPlay
+    loop
+    muted
+    className="w-full h-full object-cover"
+  ></video>
 
+      {/* طبقة التعتيم */}
       <div className="absolute inset-0 bg-black/40"></div>
 
+      {/* المحتوى النصي */}
       <div className="absolute inset-0 flex flex-col items-center justify-center text-center px-4 text-white space-y-4">
         <h1 className="text-4xl md:text-5xl font-bold">
           ذكية. مستدامة. منازل جاهزة للسكن
@@ -21,7 +23,7 @@ const Hero = () => {
           استمتع بمستقبل عيشٍ أفضل مع منازلنا الجاهزة المصممة بإتقان. تصميم مستدام يتناغم مع الراحة العصرية.
         </p>
         <Link href='/projects'
-          className="mt-4 bg-[#C09059] transition-color duration-200 hover:bg-[#ddac73] px-6 py-3 rounded-lg text-white font-semibold"
+          className="mt-4 bg-[#C09059] transition-colors duration-200 hover:bg-[#ddac73] px-6 py-3 rounded-lg text-white font-semibold"
         >
           اكتشفنا
         </Link>
