48bb864e3eb7fe37ce661e1bcdcb6354beed9470
Abedalla Emara <abedallaemara24@gmail.com>
2025-12-03 12:57:55 +0200

    last edit PUSH

--- ecohome/app/api/product/[id]/routs.js
+++ ecohome/app/api/product/[id]/routs.js
@@ -1,22 +1,22 @@
-// api/product/[id].js
 import { connectDB } from "@/lib/mongodb";
 import Product from "../../../models/Product";
+import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
   await connectDB();
   const product = await Product.findById(params.id);
-  return new Response(JSON.stringify(product || {}), { status: 200 });
+  return NextResponse.json(product || {}, { status: 200 });
 }
 
 export async function PUT(req, { params }) {
   await connectDB();
   const data = await req.json();
   const updated = await Product.findByIdAndUpdate(params.id, data, { new: true });
-  return new Response(JSON.stringify(updated), { status: 200 });
+  return NextResponse.json(updated, { status: 200 });
 }
 
 export async function DELETE(req, { params }) {
   await connectDB();
   await Product.findByIdAndDelete(params.id);
-  return new Response(JSON.stringify({ message: "Deleted Successfully" }), { status: 200 });
+  return NextResponse.json({ message: "Deleted Successfully" }, { status: 200 });
 }
