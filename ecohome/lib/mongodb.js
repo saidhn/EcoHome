625af178d83ac9e3aff1c53a06c308135aa9fc32
Abedalla Emara <abedallaemara24@gmail.com>
2025-12-03 11:42:26 +0200

    last edit PUSH

--- ecohome/lib/mongodb.js
+++ ecohome/lib/mongodb.js
@@ -1,11 +1,21 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-  if (mongoose.connections[0].readyState) return;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Missing MONGODB_URI");
+}
+
+let isConnected = false;
+
+export async function connectDB() {
+  if (isConnected) return;
+
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log("✅ MongoDB Connected");
-  } catch (err) {
-    console.error("❌ MongoDB Connection Error:", err);
+    const db = await mongoose.connect(MONGODB_URI);
+    isConnected = true;
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.log(error);
   }
-};
+}
